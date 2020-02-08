7d03503bf7b5aa15f54cf03ba44f22c26907e9bd
Norbert Melzer <timmelzer@gmail.com>
2020-02-08 16:22:51 +0100

    enable zsh

--- hosts/tux-nixos.nix
+++ hosts/tux-nixos.nix
@@ -1,9 +1,24 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 
 {
   config = {
     home.packages = [ pkgs.hydra ];
 
     services.keyleds.enable = true;
+
+    programs = {
+      zsh = {
+        enable = true;
+
+        enableCompletion = true;
+        enableAutosuggestions = true;
+
+        dotDir = ".config/zsh";
+
+        shellAliases = config.programs.zshell.aliases;
+      };
+    };
+
   };
+  # environment.pathsToLink = [ "/share/zsh" ];
 }
