cf9722b890544db91132750b327796c569ca68dc
Norbert Melzer <timmelzer@gmail.com>
2020-02-08 15:17:48 +0100

    add package option

--- modules/keyleds/default.nix
+++ modules/keyleds/default.nix
@@ -6,7 +6,17 @@ in {
   options.services.keyleds = {
     enable = lib.mkEnableOption
       "Logitech Keyboard animation for Linux — G410, G513, G610, G810, G910, GPro";
+
+    package = lib.mkOption {
+      type = lib.types.package;
+      default = pkgs.keyleds;
+      defaultText = lib.literalExample "pkgs.keyleds";
+      example = lib.literalExample "pkgs.keyleds";
+      description = ''
+        Keyleds derivation to use.
+      '';
+    };
   };
 
-  config = lib.mkIf cfg.enable { home.packages = [ pkgs.keyleds ]; };
+  config = lib.mkIf cfg.enable { home.packages = [ cfg.package ]; };
 }
