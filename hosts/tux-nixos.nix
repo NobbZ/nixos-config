0bc99fd52b7d936329d0e8fd88434a62fbf94818
Norbert Melzer <timmelzer@gmail.com>
2020-02-09 10:34:52 +0100

    enable powerlevel10k

--- hosts/tux-nixos.nix
+++ hosts/tux-nixos.nix
@@ -23,6 +23,14 @@ in {
 
         dotDir = ".config/zsh";
 
+        defaultKeymap = "emacs";
+
+        plugins = [{
+          name = "powerlevel10k";
+          src = pkgs.zsh-powerlevel10k;
+          file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+        }];
+
         shellAliases = config.programs.zshell.aliases;
       };
     };
