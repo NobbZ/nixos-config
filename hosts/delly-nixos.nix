8780154034b1467d4939bd5d047c37da779d6e17
Norbert Melzer <timmelzer@gmail.com>
2020-02-13 12:18:22 +0100

    update delly-nixos profile

--- hosts/delly-nixos.nix
+++ hosts/delly-nixos.nix
@@ -1,3 +1,24 @@
-{ ... }:
+{ pkgs, config, ... }:
 
-{ }
+{
+  config = {
+    programs.zsh = {
+      enable = true;
+
+      enableCompletion = true;
+      enableAutosuggestions = true;
+
+      dotDir = ".config/zsh";
+
+      defaultKeymap = "emacs";
+
+      plugins = [{
+        name = "powerlevel10k";
+        src = pkgs.zsh-powerlevel10k;
+        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+      }];
+
+      shellAliases = config.programs.zshell.aliases;
+    };
+  };
+}
