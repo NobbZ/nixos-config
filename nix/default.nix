c574200f8ecc09100d9652b78ad0336dce0c4c98
Norbert Melzer <timmelzer@gmail.com>
2020-02-16 14:44:21 +0100

    build 'nixfmt' from source

--- nix/default.nix
+++ nix/default.nix
@@ -2,6 +2,9 @@ let sources = import ./sources.nix { };
 
 in [
   (self: super: { inherit sources; })
+  (self: super: {
+    nixfmt = super.callPackage sources.nixfmt.outPath { installOnly = true; };
+  })
   (import sources.emacs-overlay.outPath)
   (import ./myOverlay)
 ]
