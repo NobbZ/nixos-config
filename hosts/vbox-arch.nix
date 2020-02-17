158b561c3e6656f326b12c18f4c48b80425a1349
Norbert Melzer <timmelzer@gmail.com>
2020-02-17 14:38:00 +0100

    try fix fontconfig

--- hosts/vbox-arch.nix
+++ hosts/vbox-arch.nix
@@ -4,5 +4,9 @@
   config = {
     home.packages = [ ];
     profiles.development.enable = true;
+    programs.zsh.enable = true;
+    programs.zsh.localVariables = {
+      FONTCONFIG_FILE = "${pkgs.fontconfig.out}/etc/fonts/fonts.conf";
+    };
   };
 }
