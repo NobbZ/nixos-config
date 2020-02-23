9464de737be04920f896ae6d688aa589c04bdbe5
Norbert Melzer <timmelzer@gmail.com>
2020-02-23 14:45:29 +0100

    provide derivation

--- nix/myOverlay/default.nix
+++ nix/myOverlay/default.nix
@@ -1,6 +1,7 @@
 self: super:
 
 {
+  advcp = super.callPackage (import ./advcp) { };
   asdf-vm = super.callPackage (import ./asdf) { };
   aur-tools = super.callPackage (import ./aur) { };
   keyleds = super.callPackage (import ./keyleds) { };
