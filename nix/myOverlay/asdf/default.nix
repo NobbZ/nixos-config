613da1e4e66e59e82e86074664e8cc62bfd3a3da
Norbert Melzer <timmelzer@gmail.com>
2020-02-27 13:55:27 +0000

    fix incompatibilities

--- nix/myOverlay/asdf/default.nix
+++ nix/myOverlay/asdf/default.nix
@@ -1,14 +1,19 @@
 { sources, srcOnly, lib, ... }:
-srcOnly rec {
-  pname = "asdf-vm";
+
+let
   version = sources.asdf.version;
+  pname = "asdf-vm";
+
+in srcOnly rec {
+  # pname = "asdf-vm";
+  # version = sources.asdf.version;
   name = "${pname}-${version}";
 
   src = sources.asdf;
 
-  meta = with lib; {
-    inherit (sources.asdf) homepage description;
-    license = licenses.mit;
-    maintainers = [ maintainers.nobbz ];
-  };
+  # meta = with lib; {
+  #   inherit (sources.asdf) homepage description;
+  #   license = licenses.mit;
+  #   maintainers = [ maintainers.nobbz ];
+  # };
 }
