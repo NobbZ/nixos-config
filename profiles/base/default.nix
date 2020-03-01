26bfd030390181af50610351cbb339cba0fe54f7
Norbert Melzer <timmelzer@gmail.com>
2020-03-01 10:26:35 +0100

    provide p10k configuration

--- profiles/base/default.nix
+++ profiles/base/default.nix
@@ -43,11 +43,18 @@ in {
 
         defaultKeymap = "emacs";
 
-        plugins = [{
-          name = "powerlevel10k";
-          src = pkgs.zsh-powerlevel10k;
-          file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-        }];
+        plugins = [
+          {
+            name = "powerlevel10k";
+            src = pkgs.zsh-powerlevel10k;
+            file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+          }
+          {
+            name = "powerlevel10k-config";
+            src = lib.cleanSource ./p10k-config;
+            file = "p10k.zsh";
+          }
+        ];
 
         shellAliases = config.programs.zshell.aliases;
       };
