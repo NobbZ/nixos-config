8f362e5f4b2f00e0cef1c82e8b286aaba480c5b8
Norbert Melzer <timmelzer@gmail.com>
2020-03-03 23:45:21 +0100

    provide initial key description replacements

--- modules/emacs/whichkey/default.nix
+++ modules/emacs/whichkey/default.nix
@@ -1,18 +1,58 @@
 { config, lib, pkgs, ... }:
 
+with lib;
+
 let
   cfg = config.programs.emacs.whichkey;
   enabled = config.programs.emacs.enable;
+
+  keyReplacementType = types.listOf (types.submodule ({ config, ... }: {
+    options = {
+      keys = lib.mkOption {
+        type = types.str;
+        description = ''
+          Keys that shall get a description.
+        '';
+      };
+      replace = lib.mkOption {
+        type = types.str;
+        description = ''
+          Human readable description for the keycombination
+        '';
+      };
+    };
+  }));
+
+  replacements = lib.concatStringsSep "  \n"
+    (builtins.map ({ keys, replace, ... }: ''"${keys}" "${replace}"'')
+      cfg.replacement);
 in {
   options.programs.emacs.whichkey = {
-
+    replacement = lib.mkOption { type = keyReplacementType; };
   };
 
   config = lib.mkIf enabled {
+    programs.emacs.whichkey.replacement = [
+      {
+        keys = "C-x C-f";
+        replace = "find file";
+      }
+      {
+        keys = "C-x C-s";
+        replace = "write file";
+      }
+      {
+        keys = "C-x C-c";
+        replace = "leave emacs";
+      }
+    ];
     programs.emacs.extraPackages = ep: [ ep.which-key ];
     programs.emacs.extraConfig = ''
       (which-key-mode t)
       (setq-default which-key-idle-delay 0.1)
+
+      (which-key-add-key-based-replacements
+        ${replacements})
     '';
   };
 }
