f62612d728a7f18e934cd6bcd7348873191eb3d3
Norbert Melzer <timmelzer@gmail.com>
2020-03-05 11:02:33 +0100

    enable elixir for work VM

--- hosts/nixos.nix
+++ hosts/nixos.nix
@@ -5,7 +5,7 @@
     profiles.browsing.enable = true;
     profiles.development.enable = true;
 
-    languages = { nix.enable = true; };
+    languages = { nix.enable = true; elixir.enable = true; };
 
     programs.emacs.splashScreen = false;
   };
