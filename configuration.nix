ac5fd566ff74313f8bff69f8813cc868253e3289
Norbert Melzer <timmelzer@gmail.com>
2020-03-09 22:57:49 +0100

    reduce number of builders

--- configuration.nix
+++ configuration.nix
@@ -142,11 +142,12 @@
     };
   };
 
+  nix.maxJobs = 2;
+  
   # This value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you
   # should.
   system.stateVersion = "19.09"; # Did you read the comment?
-
 }
 
