daae518bc3a312c553a85b3601cb93bbc1a29a57
Norbert Melzer <timmelzer@gmail.com>
2020-03-29 10:32:47 +0200

    enable DRM for Chrome

--- profiles/browsing/default.nix
+++ profiles/browsing/default.nix
@@ -8,6 +8,8 @@ in {
       "A profile that enables a browser for the GUI and the terminal";
   };
 
-  config =
-    lib.mkIf cfg.enable { home.packages = with pkgs; [ chromium lynx ]; };
+  config = lib.mkIf cfg.enable {
+    nixpkgs.config = { chromium = { enableWideVine = true; }; };
+    home.packages = with pkgs; [ chromium lynx ];
+  };
 }
