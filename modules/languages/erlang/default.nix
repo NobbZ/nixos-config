c1722255e0a1fdbd4cc32ff3b1d6b67cae124ca3
Norbert Melzer <timmelzer@gmail.com>
2020-04-04 01:36:51 +0200

    [erlang]: rewrite to lsp-mode

--- modules/languages/erlang/default.nix
+++ modules/languages/erlang/default.nix
@@ -9,21 +9,45 @@ in {
   };
 
   config = lib.mkIf cfg.enable {
-    programs.emacs.extraPackages = ep: [ ep.eglot ep.erlang ep.company ];
+    programs.emacs.extraPackages = ep:
+      with ep; [
+        company-lsp
+        helm-lsp
+        lsp-mode
+        lsp-origami
+        lsp-ui
+        yasnippet
+        erlang
+      ];
 
     programs.emacs.extraConfig = ''
-            ;; Configure erlang related stuff
-            (require 'eglot)
-      			(add-to-list 'eglot-server-programs
-      									 '(erlang-mode . ("${erlang-ls}/bin/erlang-ls")))
+      ;; Configure erlang related stuff
 
-      			(add-hook 'erlang-mode-hook
-      								(lambda ()
-      									(add-hook 'before-save-hook #'eglot-format-buffer nil t)
-      									(subword-mode)
-      									(eglot-ensure)
-      									(company-mode)
-      									(flymake-mode)))
-          '';
+      (yas-global-mode t)
+
+      (setq lsp-erlang-server-path "${erlang-ls}/bin/erlang_ls")
+      (setq lsp-log-io t)
+      (setq lsp-ui-sideline-enable t)
+      (setq lsp-ui-doc-enable t)
+      (setq lsp-ui-doc-position 'bottom)
+
+      (push 'company-lsp company-backends)
+
+      (eval-after-load 'erlang
+        '(define-key erlang-mode-map (kbd "C-M-i") #'company-lsp))
+
+      (add-hook 'erlang-mode-hook
+                (lambda ()
+                  (linum-mode)
+                  ('column-number-mode)
+                  (lsp)
+                  (add-hook 'before-save-hook 'lsp-format-buffer nil t)
+                  (subword-mode)
+                  (company-mode)
+                  (flymake-mode)))
+
+      (add-hook 'origami-mode-hook 'lsp-origami-mode)
+      (add-hook 'erlang-mode-hook 'origami-mode)
+    '';
   };
 }
