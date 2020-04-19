0bf07752bfe4acf88da5af5ffd223378e94beda4
Norbert Melzer <timmelzer@gmail.com>
2020-04-19 20:00:01 +0200

    [emacs] add some minor modes to nix-mode

--- modules/languages/nix/default.nix
+++ modules/languages/nix/default.nix
@@ -7,6 +7,15 @@ in {
     enable = lib.mkEnableOption "Enable support for the nix language";
   };
 
-  config =
-    lib.mkIf cfg.enable { programs.emacs.extraPackages = ep: [ ep.nix-mode ]; };
+  config = lib.mkIf cfg.enable {
+    programs.emacs.extraPackages = ep: [ ep.nix-mode ep.flycheck ];
+
+    programs.emacs.extraConfig = ''
+      (add-hook 'nix-mode-hook
+                (lambda ()
+                  (subword-mode)
+                  (company-mode)
+                  (flycheck-mode)))
+    '';
+  };
 }
