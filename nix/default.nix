292e63933284d45d64c04b9b6e0d632d2288e8d2
Norbert Melzer <timmelzer@gmail.com>
2020-05-11 18:57:30 +0200

    drop nixfmt

--- nix/default.nix
+++ nix/default.nix
@@ -2,9 +2,6 @@ let sources = import ./sources.nix { };
 
 in [
   (self: super: { inherit sources; })
-  (self: super: {
-    nixfmt = super.callPackage sources.nixfmt.outPath { installOnly = true; };
-  })
   (import sources.nixpkgs-mozilla.outPath)
   (import sources.emacs-overlay.outPath)
   (import ./myOverlay)
