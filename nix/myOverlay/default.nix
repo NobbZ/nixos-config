29d6b657a3ba1e716020ef90ff6ffe9859a0b646
Norbert Melzer <timmelzer@gmail.com>
2020-05-16 11:48:29 +0200

    fix erlang23

--- nix/myOverlay/default.nix
+++ nix/myOverlay/default.nix
@@ -1,6 +1,9 @@
 self: super:
 
-rec {
+let
+  erlLib =
+    super.callPackage <nixpkgs/pkgs/development/beam-modules/lib.nix> { };
+in rec {
   advcp = super.callPackage (import ./advcp) { };
   asdf-vm = super.callPackage (import ./asdf) { };
   aur-tools = super.callPackage (import ./aur) { };
@@ -9,19 +12,21 @@ rec {
   erlang-ls = super.callPackage (import ./erlang-ls) { };
   keyleds = super.callPackage (import ./keyleds) { };
 
-  erlangR23 = self.lib.callErlang ({ mkDerivation }:
+  erlangR23 = erlLib.callErlang ({ mkDerivation }:
     mkDerivation {
       version = "23.0";
-      sha256 = self.lib.fakeSha256;
+      sha256 = "0hw0js0man58m5mdrzrig5q1agifp92wxphnbxk1qxxbl6ccs6ls";
       prePatch = ''
         substituteInPlace make/configure.in --replace '`sw_vers -productVersion`' "''${MACOSX_DEPLOYMENT_TARGET:-10.12}"
         substituteInPlace erts/configure.in --replace '-Wl,-no_weak_imports' ""
       '';
-    });
+    }) { };
 
-  beam = super.beam.overrideAttrs (oa: {
-    packages = oa.packages // { erlangR23 = oa.packagesWith erlangR23; };
-  });
+  beam = super.beam // {
+    packages = super.beam.packages // {
+      erlangR23 = self.beam.packagesWith erlangR23;
+    };
+  };
 
   nobbzLib = (import ./lib);
 }
