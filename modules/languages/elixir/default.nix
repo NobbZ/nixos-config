c56b238970442a9849938c77474f69a1dd783ef0
Norbert Melzer <timmelzer@gmail.com>
2020-05-23 00:53:58 +0200

    [elixir] convert to new emacs config style

--- modules/languages/elixir/default.nix
+++ modules/languages/elixir/default.nix
@@ -1,31 +1,38 @@
 { config, lib, pkgs, ... }:
 
-let cfg = config.languages.elixir;
+let
+  cfg = config.languages.elixir;
 
+  inherit (pkgs) elixir-lsp;
 in {
   options.languages.elixir = {
     enable = lib.mkEnableOption "Enable support for elixir language";
   };
 
   config = lib.mkIf cfg.enable {
-    programs.emacs.extraPackages = ep: [ ep.elixir-mode ];
+    programs.emacs = {
+      lsp-mode = {
+        enable = true;
+        languages = [ "elixir" ];
+      };
 
-    programs.emacs.lsp-mode = {
-      enable = true;
-      languages = [ "elixir" ];
+      localPackages."init-elixir" = {
+        tag = "Setup elixir";
+        comments = [ ];
+        requires = [ "company" "lsp-clients" "flycheck" ];
+        packageRequires = ep: [ ep.lsp-mode ep.elixir-mode ep.company ep.flycheck ];
+        code = ''
+          (add-to-list 'exec-path "${elixir-lsp}/bin")
+          (setq lsp-clients-elixir-server-executable "elixir-ls")
+
+          (add-hook 'elixir-mode-hook
+                    (lambda ()
+                      (subword-mode)
+                      (company-mode)
+                      (flycheck-mode)
+                      (add-hook 'before-save-hook #'lsp-format-buffer nil t)))
+        '';
+      };
     };
-
-    programs.emacs.extraConfig = ''
-      ;; Confire elixir related stuff
-      (add-to-list 'exec-path "${pkgs.elixir-lsp}/bin")
-      (setq lsp-clients-elixir-server-executable "elixir-ls")
-
-      (add-hook 'elixir-mode-hook
-                (lambda ()
-                  (subword-mode)
-                  (company-mode)
-                  (flymake-mode)
-                  (add-hook 'before-save-hook #'lsp-format-buffer nil t)))
-    '';
   };
 }
