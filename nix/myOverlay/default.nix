08976f198692b1b073b2fe88add6b01590f27a91
Norbert Melzer <timmelzer@gmail.com>
2020-06-01 22:27:34 +0200

    temporary fix for keepass

--- nix/myOverlay/default.nix
+++ nix/myOverlay/default.nix
@@ -2,6 +2,13 @@ self: super:
 
 let
   erlLib = self.callPackage <nixpkgs/pkgs/development/beam-modules/lib.nix> { };
+
+  keepasspkgs = self.fetchFromGitHub {
+    owner = "wnklmnn";
+    repo = "nixpkgs";
+    rev = "e1bcd10a071ef746e1078272913048b1eef4ceee";
+    sha256 = "03bd52y51xr6fhy3r8xggq84z39p94mhipw6xyjnm1niq7iim7cw";
+  };
 in rec {
   advcp = self.callPackage (import ./advcp) { };
   asdf-vm = self.callPackage (import ./asdf) { };
@@ -11,6 +18,10 @@ in rec {
   erlang-ls = self.callPackage (import ./erlang-ls) { };
   keyleds = self.callPackage (import ./keyleds) { };
   ocaml-lsp = self.callPackage ./ocaml-ls { };
+
+  keepass =
+    self.callPackage (keepasspkgs.outPath + "/pkgs/applications/misc/keepass")
+    { };
 
   erlangR23 = erlLib.callErlang ({ mkDerivation }:
     mkDerivation {
