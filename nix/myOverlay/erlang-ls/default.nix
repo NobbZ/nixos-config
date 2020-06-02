63aa42671c8a7b0faad52e9ecf5967a54f793bfc
Norbert Melzer <timmelzer@gmail.com>
2020-06-02 23:52:04 +0200

    update erlang LS

--- nix/myOverlay/erlang-ls/default.nix
+++ nix/myOverlay/erlang-ls/default.nix
@@ -1,9 +1,14 @@
-{ beamPackages, sources, fetchFromGitHub }:
+{ beamPackages, sources, fetchFromGitHub, git, cacert }:
 
 beamPackages.rebar3Relx {
   name = "erlang-ls";
   version = sources.erlang-ls.branch;
   releaseType = "escript";
+
+  buildInputs = [ git ];
+
+  GIT_SSL_CAINFO = "${cacert}/etc/ssl/certs/ca-bundle.crt";
+  SSL_CERT_FILE = "${cacert}/etc/ssl/certs/ca-bundle.crt";
 
   src = fetchFromGitHub { inherit (sources.erlang-ls) owner repo rev sha256; };
 }
