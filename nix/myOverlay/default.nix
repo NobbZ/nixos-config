536463c8e4b0658fac181fe54a2879ebc00b6c83
Norbert Melzer <timmelzer@gmail.com>
2020-06-03 22:12:15 +0200

    remove erlang update

--- nix/myOverlay/default.nix
+++ nix/myOverlay/default.nix
@@ -21,21 +21,5 @@ in rec {
     self.callPackage (keepasspkgs.outPath + "/pkgs/applications/misc/keepass")
     { };
 
-  erlangR23 = erlLib.callErlang ({ mkDerivation }:
-    mkDerivation {
-      version = "23.0";
-      sha256 = "0hw0js0man58m5mdrzrig5q1agifp92wxphnbxk1qxxbl6ccs6ls";
-      prePatch = ''
-        substituteInPlace make/configure.in --replace '`sw_vers -productVersion`' "''${MACOSX_DEPLOYMENT_TARGET:-10.12}"
-        substituteInPlace erts/configure.in --replace '-Wl,-no_weak_imports' ""
-      '';
-    }) { };
-
-  beam = super.beam // {
-    packages = super.beam.packages // {
-      erlangR23 = self.beam.packagesWith erlangR23;
-    };
-  };
-
   nobbzLib = (import ./lib);
 }
