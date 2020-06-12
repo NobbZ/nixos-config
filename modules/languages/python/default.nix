9c32ef76fd39996984a65a099720271fefce1189
Norbert Melzer <timmelzer@gmail.com>
2020-06-12 10:34:35 +0200

    I think this fully enables the MS-python-LS

--- modules/languages/python/default.nix
+++ modules/languages/python/default.nix
@@ -6,6 +6,15 @@ let
   mspyls = "${pkgs.python-language-server}/bin/python-language-server";
 
   lsBin = if cfg.useMS then mspyls else pyls;
+
+  lsHook = if cfg.useMS then
+    "(add-hook 'python-mode-hook (lambda () (require 'lsp-python-ms) (lsp)))"
+  else
+    "";
+  lsExec = if cfg.useMS then
+    ''(setq lsp-python-ms-executable "${lsBin}")''
+  else
+    ''(setq lsp-pyls-server-command '("${lsBin}"))'';
 in {
   options.languages.python = {
     enable = lib.mkEnableOption "Enable support for python language";
@@ -15,12 +24,21 @@ in {
   config = lib.mkIf cfg.enable {
     programs.emacs.lsp-mode = {
       enable = true;
-      languages = [ "python" ];
+      languages = if cfg.useMS then [ ] else [ "python" ];
     };
 
-    programs.emacs.extraConfig = ''
-      ;; Configure python related stuff
-      (setq lsp-pyls-server-command '("${lsBin}"))
-    '';
+    programs.emacs.localPackages."init-python" = {
+      tag = "Setup and prepare the python language modes";
+      comments = [ ];
+      requires = [ "lsp-mode" ];
+      packageRequires = (ep:
+        [ (config.programs.emacs.localPackages."init-lsp".packageRequires ep) ]
+        ++ (if cfg.useMS then [ ep.lsp-python-ms ] else [ ]));
+      code = ''
+        ${lsHook}
+
+        ${lsExec}
+      '';
+    };
   };
 }
