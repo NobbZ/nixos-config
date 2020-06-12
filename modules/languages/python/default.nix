e55be5eb5ef3b0d69fd0397334c3f29bab5ce5aa
Norbert Melzer <timmelzer@gmail.com>
2020-06-12 09:34:42 +0200

    allow to use MS pythonon LS

--- modules/languages/python/default.nix
+++ modules/languages/python/default.nix
@@ -2,11 +2,14 @@
 
 let
   cfg = config.languages.python;
-  pyls = pkgs.python37Packages.python-language-server;
+  pyls = "${pkgs.python37Packages.python-language-server}/bin/pyls";
+  mspyls = "${pkgs.python-language-server}/bin/python-language-server";
 
+  lsBin = if cfg.useMS then mspyls else pyls;
 in {
   options.languages.python = {
     enable = lib.mkEnableOption "Enable support for python language";
+    useMS = lib.mkEnableOption "Use MS language server rather than palantirs";
   };
 
   config = lib.mkIf cfg.enable {
@@ -17,7 +20,7 @@ in {
 
     programs.emacs.extraConfig = ''
       ;; Configure python related stuff
-      (setq lsp-pyls-server-command '("${pyls}/bin/pyls"))
+      (setq lsp-pyls-server-command '("${lsBin}"))
     '';
   };
 }
