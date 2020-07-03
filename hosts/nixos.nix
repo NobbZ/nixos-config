374674e2bcaf0b52d5494ff2b629ee9ca797e4d9
Norbert Melzer <timmelzer@gmail.com>
2020-07-03 14:18:22 +0200

    enable ocaml for nixos machine

--- hosts/nixos.nix
+++ hosts/nixos.nix
@@ -5,7 +5,8 @@
     profiles.browsing.enable = true;
     profiles.development.enable = true;
 
-    enabledLanguages = [ "elixir" "go" "lua" "nix" "python" "terraform" ];
+    enabledLanguages =
+      [ "elixir" "go" "lua" "nix" "python" "terraform" "ocaml" ];
 
     languages.python.useMS = true;
 
