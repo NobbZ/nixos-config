91b4428722e36c5c648ef115ee9105ebbdcc1c1c
Norbert Melzer <timmelzer@gmail.com>
2020-07-05 12:22:27 +0200

    add shell environment variables

--- shell.nix
+++ shell.nix
@@ -1,8 +1,22 @@
 let
-  pkgs = import <nixpkgs> {
+  sources = import ./nix/sources.nix;
+  unstable = sources.nixpkgs-unstable;
+  stable = sources.nixos-stable;
+
+  pkgs = import unstable {
     overlays = import ./nix;
     config = { };
   };
 
+  home-manager = (import sources.home-manager { inherit pkgs; }).home-manager;
+
   inherit (pkgs) niv lefthook;
-in pkgs.mkShell { buildInputs = [ niv lefthook ]; }
+in pkgs.mkShell {
+  name = "home-manager-shell";
+
+  buildInputs = [ niv lefthook home-manager ];
+
+  NIX_PATH =
+    "nixpkgs=${unstable}:nixos=${stable}:home-manager=${sources.home-manager}";
+  HOME_MANAGER_CONFIG = "./home.nix";
+}
