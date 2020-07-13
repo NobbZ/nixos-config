3ac48bd5f005c6de29dfb97634628786810259a0
Norbert Melzer <timmelzer@gmail.com>
2020-07-13 16:06:09 +0200

    use lxterminal on delly

--- hosts/delly-nixos.nix
+++ hosts/delly-nixos.nix
@@ -1,10 +1,13 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 
 {
   config = {
     profiles.browsing.enable = true;
     profiles.development.enable = true;
     profiles.home-office.enable = true;
+
+    xsession.windowManager.awesome.terminalEmulator =
+      "${pkgs.lxterminal}/bin/lxterminal";
 
     enabledLanguages = [ "clojure" "nix" "elixir" "erlang" "python" "rust" ];
 
