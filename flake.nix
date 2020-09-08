90a5916dbdff345e4db679879f4887eb609d2818
Norbert Melzer <timmelzer@gmail.com>
2020-09-08 22:37:43 +0200

    add dev shell

--- flake.nix
+++ flake.nix
@@ -2,6 +2,11 @@
   inputs.nixpkgs.url = "github:nixos/nixpkgs-channels/nixos-20.03";
 
   outputs = { self, nixpkgs }: {
+    devShell.x86_64-linux = let pkgs = nixpkgs.legacyPackages.x86_64-linux; in pkgs.mkShell {
+      name = "nixos-builder";
+      buildInputs = [ pkgs.gnumake ];
+    };
+
     nixosConfigurations = {
       tux-nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
