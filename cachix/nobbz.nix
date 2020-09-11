03ff3333754ce8dd090556f00cd839db332e4a76
Norbert Melzer <timmelzer@gmail.com>
2020-09-11 14:43:01 +0200

    add nobbz cache

---
+++ cachix/nobbz.nix
@@ -0,0 +1,12 @@
+
+{
+  nix = {
+    binaryCaches = [
+      "https://nobbz.cachix.org"
+    ];
+    binaryCachePublicKeys = [
+      "nobbz.cachix.org-1:fODxpqE4ni+pFDSuj2ybYZbMUjmxNTjA7rtUNHW61Ok="
+    ];
+  };
+}
+    
