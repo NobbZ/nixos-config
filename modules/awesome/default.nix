cc26f6f101908081b0c1e483a7317d9319f7faf9
Norbert Melzer <timmelzer@gmail.com>
2020-09-15 14:01:51 +0200

    format file

--- modules/awesome/default.nix
+++ modules/awesome/default.nix
@@ -1,34 +1,35 @@
 { config, lib, pkgs, ... }:
-let cfg = config.xsession.windowManager.awesome;
+let
+  cfg = config.xsession.windowManager.awesome;
 
-    errorHandling = ''
-      -- {{{ Error handling
-      if awesome.startup_errors then
-         naughty.notify({
-               preset = naughty.config.presets.critical,
-               title = "Oops, there were errors during startup!",
-               text = awesome.startup_errors
-         })
-      end
+  errorHandling = ''
+    -- {{{ Error handling
+    if awesome.startup_errors then
+       naughty.notify({
+             preset = naughty.config.presets.critical,
+             title = "Oops, there were errors during startup!",
+             text = awesome.startup_errors
+       })
+    end
 
-      do
-         local in_error = false
-         awesome.connect_signal(
-            "debug::error",
-            function (err)
-               if in_error then return end
-               in_error = true
+    do
+       local in_error = false
+       awesome.connect_signal(
+          "debug::error",
+          function (err)
+             if in_error then return end
+             in_error = true
 
-               naughty.notify({
-                     preset = naughty.config.presets.critical,
-                     title = "Oops, an error happened!",
-                     text = tostring(err)
-               })
-               in_error = false
-         end)
-      end
-      -- }}}
-    '';
+             naughty.notify({
+                   preset = naughty.config.presets.critical,
+                   title = "Oops, an error happened!",
+                   text = tostring(err)
+             })
+             in_error = false
+       end)
+    end
+    -- }}}
+  '';
 in
 {
   options.xsession.windowManager.awesome = {
