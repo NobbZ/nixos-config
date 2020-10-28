00e87fefc8a7e24492579671c109f71bcd7ad791
Norbert Melzer <timmelzer@gmail.com>
2020-10-28 15:03:30 +0100

    extract the hosts

--- flake.nix
+++ flake.nix
@@ -1,20 +1,20 @@
 {
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-20.09";
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs }@inputs: {
     devShell.x86_64-linux = let
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
     in
       pkgs.mkShell {
         name = "nixos-builder";
-        buildInputs = [ pkgs.gnumake pkgs.nixpkgs-fmt ];
+        buildInputs = [ pkgs.gnumake pkgs.nixpkgs-fmt pkgs.git ];
       };
 
     nixosModules = import ./modules;
 
-    nixosConfigurations = {
-      tux-nixos = nixpkgs.lib.nixosSystem (import ./hosts/tux-nixos.nix { inherit self nixpkgs; });
-      nixos = nixpkgs.lib.nixosSystem (import ./hosts/nixos.nix { inherit self nixpkgs; });
-    };
+    nixosConfigurations = import ./hosts inputs; # with nixpkgs.lib; {
+    #   tux-nixos = nixosSystem (import ./hosts/tux-nixos.nix inputs);
+    #   nixos = nixosSystem (import ./hosts/nixos.nix inputs);
+    # };
   };
 }
