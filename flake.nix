44897e7917cb7285e0d2ead45952bec686aa5575
Norbert Melzer <timmelzer@gmail.com>
2020-10-28 12:20:09 +0100

    consolidate nixpkgs inputs, as both pointed to 20.09 branch

--- flake.nix
+++ flake.nix
@@ -1,8 +1,7 @@
 {
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-20.09";
-  inputs.nixpkgs-2009.url = "github:nixos/nixpkgs/nixos-20.09";
 
-  outputs = { self, nixpkgs, nixpkgs-2009 }: {
+  outputs = { self, nixpkgs }: {
     devShell.x86_64-linux = let
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
     in
@@ -21,7 +20,7 @@
 
     nixosConfigurations = {
       tux-nixos = nixpkgs.lib.nixosSystem (import ./hosts/tux-nixos.nix { inherit self nixpkgs; });
-      nixos = nixpkgs.lib.nixosSystem (import ./hosts/nixos.nix { nixpkgs = nixpkgs-2009; inherit self; });
+      nixos = nixpkgs.lib.nixosSystem (import ./hosts/nixos.nix { inherit self nixpkgs; });
     };
   };
 }
