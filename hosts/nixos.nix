721a042df405f0d6b017c37343f039cc39b8ae9a
Norbert Melzer <timmelzer@gmail.com>
2020-12-01 11:04:21 +0100

    [nixos] provide service that starts imwheel and keeps it running
    FIX #3

--- hosts/nixos.nix
+++ hosts/nixos.nix
@@ -12,5 +12,29 @@
     programs.emacs.splashScreen = false;
 
     home.packages = [ pkgs.minikube ];
+
+    systemd.user.services = {
+      imwheel = {
+        Unit = {
+          Description = "IMWheel";
+          Wants = [ "display-manager.service" ];
+          After = [ "display-manager.service" ];
+        };
+
+        Service = {
+          Type = "simple";
+          Environment = [ "XAUTHORITY=%h/.Xauthority" ];
+          ExecStart = "${pkgs.imwheel}/bin/imwheel -d";
+          ExecStop = "${pkgs.procps}/bin/pkill imwheel";
+
+          Restart = "on-failure";
+          RestartSec = "10";
+        };
+
+        Install = {
+          WantedBy = [ "graphical-session.target" ];
+        };
+      };
+    };
   };
 }
