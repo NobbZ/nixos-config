c11de32d07ffd75797641b6d37668966afba9a28
Norbert Melzer <timmelzer@gmail.com>
2020-12-04 07:47:43 +0100

    enable virt-manager on tux

--- hosts/legacy/tux-nixos.nix
+++ hosts/legacy/tux-nixos.nix
@@ -61,7 +61,7 @@
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-  environment.systemPackages = with pkgs; [ ];
+  environment.systemPackages = with pkgs; [ virt-manager ];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
