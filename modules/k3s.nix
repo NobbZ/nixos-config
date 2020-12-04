429d5427260136f6b7b446d1797ade1edf834bdd
Norbert Melzer <timmelzer@gmail.com>
2020-12-04 07:32:13 +0100

    revert k3s

--- modules/k3s.nix
+++ modules/k3s.nix
@@ -3,13 +3,12 @@
 {
   services.k3s = {
     enable = true;
-    # package = unstable.legacyPackages.x86_64-linux.k3s;
+    package = unstable.legacyPackages.x86_64-linux.k3s;
     role = "server";
     docker = false;
-    extraFlags = "--write-kubeconfig-mode 644 --disable traefik";
+    extraFlags = "--write-kubeconfig-mode 644 --disable traefik --default-local-storage-path /opt/local-path-provisioner";
   };
 
-  networking.firewall = {
-    enable = false; # TODO: re-enable firewall once learned how to do with k3s
-  };
+  #  TODO: re-enable firewall once learned how to do with k3s
+  # systemd.services.firewall.enable = false;
 }
