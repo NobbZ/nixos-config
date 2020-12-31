cf7b752f78f272738d457ee5121a8842c9b6af01
Norbert Melzer <timmelzer@gmail.com>
2020-12-31 19:37:59 +0100

    make system recognized by pkgs

--- hosts/tux-nixos.nix
+++ hosts/tux-nixos.nix
@@ -1,6 +1,9 @@
 { pkgs, ... }:
 let
-  nixos = import pkgs.inputs.nixpkgs-stable { system = "x86_64-linux"; config.allowUnfree = true; };
+  nixos = import pkgs.inputs.nixpkgs-stable {
+    inherit (pkgs) system;
+    config.allowUnfree = true;
+  };
 
   keepassWithPlugins =
     pkgs.keepass.override { plugins = [ pkgs.keepass-keepasshttp ]; };
