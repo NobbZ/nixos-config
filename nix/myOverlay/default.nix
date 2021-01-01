bd7bdd06de36ccaa975185e0731feec834f608ac
Norbert Melzer <timmelzer@gmail.com>
2021-01-01 13:16:49 +0100

    make overlay use correct names

--- nix/myOverlay/default.nix
+++ nix/myOverlay/default.nix
@@ -1,28 +1,28 @@
-self: super:
+final: prev:
 let
-  keepasspkgs = self.fetchFromGitHub {
+  keepasspkgs = final.fetchFromGitHub {
     owner = "wnklmnn";
     repo = "nixpkgs";
     rev = "e1bcd10a071ef746e1078272913048b1eef4ceee";
     sha256 = "03bd52y51xr6fhy3r8xggq84z39p94mhipw6xyjnm1niq7iim7cw";
   };
 
-  stable = import super.inputs.nixpkgs-stable { inherit (super) system; };
+  stable = import prev.inputs.nixpkgs-stable { inherit (prev) system; };
 in
 rec {
-  advcp = self.callPackage (import ./advcp) { };
-  elixir-lsp = self.beam.packages.erlang.callPackage (import ./elixir-lsp) {
+  advcp = final.callPackage (import ./advcp) { };
+  elixir-lsp = prev.beam.packages.erlang.callPackage (import ./elixir-lsp) {
     rebar3 = stable.beam.packages.erlang.rebar3;
   };
-  erlang-ls = super.beam.packages.erlang.callPackage (import ./erlang-ls) {
+  erlang-ls = prev.beam.packages.erlang.callPackage (import ./erlang-ls) {
     # beamPackages = super.beam.packages.erlangR21;
   };
-  keyleds = self.callPackage (import ./keyleds) { };
-  ocaml-lsp = self.callPackage ./ocaml-ls { };
-  rofi-unicode = self.callPackage ./rofi-unicode.nix { };
-  nix-zsh-completions = super.nix-zsh-completions.overrideAttrs (_: {
+  keyleds = final.callPackage (import ./keyleds) { };
+  ocaml-lsp = final.callPackage ./ocaml-ls { };
+  rofi-unicode = final.callPackage ./rofi-unicode.nix { };
+  nix-zsh-completions = prev.nix-zsh-completions.overrideAttrs (_: {
     version = "overlay";
-    src = self.fetchFromGitHub {
+    src = final.fetchFromGitHub {
       owner = "Ma27";
       repo = "nix-zsh-completions";
       rev = "939c48c182e9d018eaea902b1ee9d00a415dba86";
@@ -31,15 +31,15 @@ rec {
   });
 
   keepass =
-    self.callPackage (keepasspkgs.outPath + "/pkgs/applications/misc/keepass")
+    final.callPackage (keepasspkgs.outPath + "/pkgs/applications/misc/keepass")
       { };
 
   nobbzLib = (import ./lib);
 } //
-(super.lib.foldr
+(prev.lib.foldr
   (v: acc:
     acc // {
-      "julia_${v}" = super."julia_${v}".overrideAttrs (_: { doCheck = false; });
+      "julia_${v}" = prev."julia_${v}".overrideAttrs (_: { doCheck = false; });
     }))
   { }
   [ "15" "13" "10" ]
