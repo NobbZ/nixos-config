74f41856cff1b94ae005acb1bbc6e43b08f73b8d
Norbert Melzer <timmelzer@gmail.com>
2021-01-17 01:35:38 +0100

    add ts-helper cache

---
+++ modules/cachix/ts-helper.nix
@@ -0,0 +1,10 @@
+{
+  nix = {
+    binaryCaches = [
+      "https://ts-helper.cachix.org"
+    ];
+    binaryCachePublicKeys = [
+      "ts-helper.cachix.org-1:l9XtzxPqlR/lKsKpTS+DcCn4cCuYiUSgGzIsLF3vz9Q="
+    ];
+  };
+}
