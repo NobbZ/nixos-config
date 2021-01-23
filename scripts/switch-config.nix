c4ee2398f07c69fa6fe8c8a07ae0710c1e51cbc0
Norbert Melzer <timmelzer@gmail.com>
2021-01-23 10:31:02 +0100

    [switch-config]: enable pushing and switching to remote

--- scripts/switch-config.nix
+++ scripts/switch-config.nix
@@ -3,14 +3,44 @@
 writeShellScriptBin "switch-config.sh" ''
   set -ex
 
-  name=$(${nettools}/bin/hostname)
+  name=""
+  targetHost=""
+
+  while (( $# > 0 )); do
+    i=$1; shift 1
+
+    case "$i" in
+      --targetHost)
+        targetHost=$1; shift 1
+        ;;
+
+      --name)
+        name=$1; shift 1
+        ;;
+    esac
+  done
+
+  if [ -z "$name" ]; then
+    if [ -z "$targetHost" ]; then
+      name=$(${nettools}/bin/hostname)
+    else
+      printf "--name needs to be specified"
+    fi
+  fi
   outLink=$(mktemp -d)/result-$name
 
   ${nixUnstable}/bin/nix build -L \
      --out-link "$outLink" \
      .#$name
 
-  echo "Will activate, please enter your password to elevate"
-  sudo $outLink/bin/switch-to-configuration switch
+  if [ -z "$targetHost" ]; then
+    echo "Will activate, please enter your password to elevate"
+    sudo $outLink/bin/switch-to-configuration switch
+  else
+    storePath=$(readlink "$outLink")
+    nix copy --to "ssh://root@$targetHost" "$storePath"
+    ssh "root@$targetHost" "$storePath/bin/switch-to-configuration" switch
+  fi
+
   rm $outLink
 ''
