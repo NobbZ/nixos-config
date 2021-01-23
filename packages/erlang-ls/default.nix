368624dec74c74d29425c6f087af4b58522d773f
Norbert Melzer <timmelzer@gmail.com>
2021-01-23 23:06:24 +0100

    erlang-ls: fix 0.9.0

--- packages/erlang-ls/default.nix
+++ packages/erlang-ls/default.nix
@@ -1,30 +1,54 @@
-{ rebar3Relx, fetchFromGitHub, git, cacert }:
+{ stdenv, erlang, rebar3, lib, rebar3Relx, fetchFromGitHub, git, cacert }:
 let
   source = builtins.fromJSON (builtins.readFile ./source.json);
+
+  fetchRebar3Deps =
+    { name, version, sha256, src, meta ? { } }:
+
+    stdenv.mkDerivation ({
+      name = "rebar-deps-${name}-${version}";
+
+      buildInputs = [ git cacert ];
+
+      phases = [ "downloadPhase" "installPhase" ];
+
+      downloadPhase = ''
+        cp ${src} .
+        HOME='.' DEBUG=1 ${rebar3}/bin/rebar3 get-deps
+      '';
+
+      installPhase = ''
+        mkdir -p "$out/_checkouts"
+        for i in ./_build/default/lib/* ; do
+          echo "$i"
+          rm -rf "$i"/.git
+          cp -R "$i" "$out/_checkouts"
+        done
+      '';
+
+      outputHashAlgo = "sha256";
+      outputHashMode = "recursive";
+      outputHash = sha256;
+
+      # impureEnvVars = lib.fetchers.proxyImpureEnvVars;
+      inherit meta;
+    });
 in
 rebar3Relx rec {
   name = "erlang-ls";
-  version = "0.4.1"; # TODO: get in otheriwse
+  version = "${source.version}-${erlang.version}";
   releaseType = "escript";
 
-  buildInputs = [ git ];
+  checkouts = fetchRebar3Deps {
+    inherit name version;
+    src = "${src}/rebar.lock";
+    sha256 = "sha256-QNSnRGHHWMlngZfHugBdF0FpyuvcuTIgoQKrUWNIfxE=";
+  };
 
-  GIT_SSL_CAINFO = "${cacert}/etc/ssl/certs/ca-bundle.crt";
-  SSL_CERT_FILE = "${cacert}/etc/ssl/certs/ca-bundle.crt";
-
-  # checkouts = beamPackages.fetchRebar3Deps {
-  #   inherit name version;
-  #   src = "${src}/rebar.lock";
-  #   sha256 = pkgs.lib.fakeSha256;
-  # };
-
-  preBuild = ''
-    HOME=$(pwd) rebar3 get-deps
+  postPatch = ''
+    substituteInPlace src/erlang_ls.app.src \
+      --replace '{vsn, git}' '{vsn, "${version}"}'
   '';
-
-  outputHashMode = "recursive";
-  outputHashAlgo = "sha256";
-  outputHash = "0b2mkyqplhz46ldyhpggd1z34kipj59cmh9a469i02zcfdjl3xhc";
 
   src = fetchFromGitHub {
     name = "source-${name}-${version}";
