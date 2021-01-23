0f3c6e01b14cd36540f34be1b94667bbe1ccc664
Norbert Melzer <timmelzer@gmail.com>
2021-01-23 11:30:15 +0100

    [switch-config]: provide rudimentary targetHost functionality

--- scripts/switch-config/default.nix
+++ scripts/switch-config/default.nix
@@ -1,12 +1,52 @@
-{ writeShellScript, nettools, ... }:
+{ writeShellScript, nixUnstable, nettools, ... }:
 
 writeShellScript "switch-config.sh" ''
   set -ex
 
-  name=$(${nettools}/bin/hostname)
+  name=""
+  user="$USER"
+  targetHost=""
+
+  while (( $# > 0 )); do
+    i=$1; shift 1
+
+    case "$i" in
+      --targetHost)
+        targetHost=$1; shift 1
+        ;;
+
+      --name)
+        name=$1; shift 1
+        ;;
+
+      --user)
+        user=$1; shift 1
+        ;;
+    esac
+  done
+
+  if [ -z "$name" ]; then
+    if [ -z "$targetHost" ]; then
+      name=$(${nettools}/bin/hostname)
+    else
+      printf "--name needs to be specified"
+      exit
+    fi
+  fi
+
   outLink=$(mktemp -d)/result-$name
 
-  nix build -L --out-link "$outLink" ".#$name"
-  $outLink/activate
+  ${nixUnstable}/bin/nix build -L \
+    --out-link "$outLink" \
+    ".#$name"
+
+  if [ -z "$targetHost" ]; then
+    $outLink/activate
+  else
+    storePath=$(readlink "$outLink")
+    nix copy --to "ssh://root@$targetHost" "$storePath"
+    ssh "$user@$targetHost" "$storePath/activate"
+  fi
+
   rm $outLink
 ''
