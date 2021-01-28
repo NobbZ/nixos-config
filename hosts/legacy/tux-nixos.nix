1bb2957cd6a8463e8afdfc3e78e3cbd32b44f87f
Norbert Melzer <timmelzer@gmail.com>
2021-01-28 07:46:48 +0100

    tux: experiment with WoL

--- hosts/legacy/tux-nixos.nix
+++ hosts/legacy/tux-nixos.nix
@@ -193,6 +193,13 @@
     }
   ];
 
+  services.wakeonlan.interfaces = [
+    {
+      interface = "enp5s0f2";
+      method = "magicpacket";
+    }
+  ];
+
   # This value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you
