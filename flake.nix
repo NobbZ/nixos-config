3a5dca168d4e17a46bfa62aed73a8802172dc425
Norbert Melzer <timmelzer@gmail.com>
2021-04-12 00:14:19 +0200

    move host packages around a bit

--- flake.nix
+++ flake.nix
@@ -4,36 +4,35 @@
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils, ... }@inputs: {
-    devShell.x86_64-linux =
-      nixpkgs.legacyPackages.x86_64-linux.callPackage
-        ./packages/devShell.nix
-        { };
+  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      nixos = pkgs.recurseIntoAttrs {
+        configs = pkgs.recurseIntoAttrs (builtins.mapAttrs
+          (_: hostConfig: hostConfig.config.system.build.toplevel)
+          self.nixosConfigurations);
+      };
+    in
+    {
+      devShell.x86_64-linux =
+        nixpkgs.legacyPackages.x86_64-linux.callPackage
+          ./packages/devShell.nix
+          { };
 
-    nixosModules = import ./nixos/modules;
-    nixosConfigurations = import ./nixos/hosts inputs;
+      nixosModules = import ./nixos/modules;
+      nixosConfigurations = import ./nixos/hosts inputs;
 
-    checks.x86_64-linux =
-      let
-        pkgs = nixpkgs.legacyPackages.x86_64-linux;
-        nixos = pkgs.recurseIntoAttrs {
-          configs = pkgs.recurseIntoAttrs
-            (builtins.mapAttrs
-              (_: hostConfig: hostConfig.config.system.build.toplevel)
-              self.nixosConfigurations);
-        };
-      in
-      flake-utils.lib.flattenTree (pkgs.recurseIntoAttrs { inherit nixos; });
+      checks.x86_64-linux =
+        flake-utils.lib.flattenTree (pkgs.recurseIntoAttrs { inherit nixos; });
 
-    packages.x86_64-linux = (import ./scripts inputs)
-      // builtins.mapAttrs
-      (_: hostConfig:
-        hostConfig.config.system.build.toplevel)
-      self.nixosConfigurations;
+      packages.x86_64-linux = (import ./scripts inputs)
+        // flake-utils.lib.flattenTree (pkgs.recurseIntoAttrs {
+        inherit nixos;
+      });
 
-    apps.x86_64-linux = {
-      build = { type = "app"; program = "${self.packages.x86_64-linux.build-config}/bin/build-config.sh"; };
-      switch = { type = "app"; program = "${self.packages.x86_64-linux.switch-config}/bin/switch-config.sh"; };
+      apps.x86_64-linux = {
+        build = { type = "app"; program = "${self.packages.x86_64-linux.build-config}/bin/build-config.sh"; };
+        switch = { type = "app"; program = "${self.packages.x86_64-linux.switch-config}/bin/switch-config.sh"; };
+      };
     };
-  };
 }
