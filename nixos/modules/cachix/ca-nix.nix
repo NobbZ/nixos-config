3a32118876f836a76630c8fa5a21af07bb226c9c
Norbert Melzer <timmelzer@gmail.com>
2021-05-07 07:52:12 +0200

    add additional substitute

---
+++ nixos/modules/cachix/ca-nix.nix
@@ -0,0 +1,10 @@
+{
+  nix = {
+    binaryCaches = [
+      "https://cache.ngi0.nixos.org/"
+    ];
+    binaryCachePublicKeys = [
+      "cache.ngi0.nixos.org-1:KqH5CBLNSyX184S9BKZJo1LxrxJ9ltnY2uAs5c/f1MA="
+    ];
+  };
+}
