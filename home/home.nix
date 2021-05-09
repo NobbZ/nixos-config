9d32097b7e73bd787d4df614dac203f9a22d941e
Norbert Melzer <timmelzer@gmail.com>
2021-05-09 09:40:35 +0200

    use upstream lefthook

--- home/home.nix
+++ home/home.nix
@@ -37,39 +37,7 @@ in
         p.fira-code
         p.cascadia-code
 
-        (p.callPackage
-          ({ lib, buildGoModule, fetchFromGitHub }:
-
-            # Currently `buildGo114Module` is passed as `buildGoModule` from
-            # `../default.nix`. Please remove the fixed 1.14 once a new release has been
-            # made and the issue linked below has been closed upstream.
-
-            # https://github.com/Arkweid/lefthook/issues/151
-
-            buildGoModule rec {
-              pname = "lefthook";
-              version = "065b24f-git";
-
-              src = fetchFromGitHub {
-                name = "${pname}-${version}-source";
-                rev = "065b24f";
-                owner = "Arkweid";
-                repo = "lefthook";
-                sha256 = "sha256-GCZvrG9SK1rR8lcH7aiy5yASDz83TfPWm5n4Ub5i4/M=";
-              };
-
-              vendorSha256 = "sha256-XR7xJZfgt0Hx2DccdNlwEmuduuVU8IBR0pcIUyRhdko=";
-
-              doCheck = false;
-
-              meta = with lib; {
-                description = "Fast and powerful Git hooks manager for any type of projects";
-                homepage = "https://github.com/Arkweid/lefthook";
-                license = licenses.mit;
-                maintainers = with maintainers; [ rencire ];
-              };
-            })
-          { })
+        p.lefthook
       ];
 
     stateVersion = "20.09";
