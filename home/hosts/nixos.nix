98995006ccf382e18c6473d06ed149b49b7b65d8
Norbert Melzer <timmelzer@gmail.com>
2021-05-26 18:01:50 +0200

    nixos: quickfix build

--- home/hosts/nixos.nix
+++ home/hosts/nixos.nix
@@ -1,16 +1,18 @@
-{ pkgs, stable, self, ... }:
+{ pkgs, lib, stable, self, ... }:
 
 {
   config = {
     activeProfiles = [ "browsing" "development" ];
 
     enabledLanguages =
-      [ "elixir" "go" "lua" "nix" "python" "terraform" ];
+      [ "go" "lua" "nix" "python" "terraform" ];
 
     # languages.python.useMS = true;
 
     programs.emacs.splashScreen = false;
     programs.emacs.extraPackages = ep: [ ep.robot-mode ];
+
+    programs.advancedCopy.enable = lib.mkForce false;
 
     services.gnome-keyring.enable = true;
 
