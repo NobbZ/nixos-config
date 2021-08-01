f869f70f4b6d0f6405a39ab204d12f97208a2e5f
Norbert Melzer <timmelzer@gmail.com>
2021-08-01 10:17:25 +0200

    move cachix over

--- nixos/modules/cachix/default.nix
+++ nixos/modules/cachix/default.nix
@@ -1,16 +1,13 @@
 { config, pkgs, lib, ... }:
 let
-  folder = ./cachix;
+  folder = ./caches;
   toImport = name: value: folder + ("/" + name);
   filterCaches = key: value: value == "regular" && lib.hasSuffix ".nix" key;
   imports = lib.mapAttrsToList toImport (lib.filterAttrs filterCaches (builtins.readDir folder));
-
-  hostname = config.networking.hostName;
-  caEnabled = __elem hostname [ ];
 in
 {
   inherit imports;
-  nix.binaryCaches = lib.mkDefault (lib.optional caEnabled "https://cache.nixos.org/");
+  nix.binaryCaches = [ "https://cache.nixos.org/" ];
 
   environment.systemPackages = [ pkgs.cachix ];
 }
--- nixos/modules/default.nix
+++ nixos/modules/default.nix
@@ -1,4 +1,5 @@
 {
   flake = import ./flake.nix;
   nix = import ./nix.nix;
+  cachix = import ./cachix;
 }
