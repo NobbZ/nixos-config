8b85eb4e283ac029453d0e0af34d2ceaa4146b41
Norbert Melzer <timmelzer@gmail.com>
2021-08-09 08:03:54 +0200

    current host and user switch

--- apps/switch/switch.mjs
+++ apps/switch/switch.mjs
@@ -1,18 +1,36 @@
 #!/usr/bin/env zx
 
+const info = (line) => console.log(chalk.yellow(line));
+
+const trim = (str) => str.replace(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, '');
+
 console.log(process.argv.slice(2))
 
 console.log(argv)
 
-console.log("loading nixos names")
+info("loading nixos names")
 
 const nixosConfigs = JSON.parse(await fs.readFile(argv.nixos))
 
-console.log("loading home manager names")
+info("loading home manager names")
 
 const homeConfigs = JSON.parse(await fs.readFile(argv.home))
 
-console.log("finished reading inputs")
+info("reading hostname");
 
-nixosConfigs.map(val => console.log(val))
-homeConfigs.map(val => console.log(val))
+const hostName = (await fs.readFile("/etc/hostname", "utf8")).trim()
+
+info("reading user");
+
+const user = process.env.USER.trim();
+
+info("finished reading inputs")
+
+const targets = [
+    `.#nixos/config/${hostName}`,
+    `.#home/config/${user}@${hostName}`,
+]
+
+await $`nix build --keep-going -L ${targets}`
+
+await $`sudo nixos-rebuild switch --flake .#${hostName}`
