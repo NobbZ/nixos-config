820d77b83a65853bab284af7ff4a94566c0ac7d9
Norbert Melzer <timmelzer@gmail.com>
2021-12-02 13:44:32 +0100

    add more channel mappings

--- nixos/modules/flake.nix
+++ nixos/modules/flake.nix
@@ -1,7 +1,10 @@
-{ config, pkgs, unstable, nix, lib, ... }:
+{ config, pkgs, unstable, nixpkgs-2105, nixpkgs-2111, nix, lib, ... }:
 
 let
-  nixpkgsPath = "/etc/nixpkgs/channels/nixpkgs";
+  base = "/etc/nixpkgs/channels";
+  nixpkgsPath = "${base}/nixpkgs";
+  nixpkgs2105Path = "${base}/nixpkgs2105";
+  nixpkgs2111Path = "${base}/nixpkgs2111";
 in
 {
   options.nix.flakes.enable = lib.mkEnableOption "nix flakes";
@@ -12,14 +15,21 @@ in
       experimentalFeatures = "nix-command flakes";
 
       registry.nixpkgs.flake = unstable;
+      registry.nixpkgs2105.flake = nixpkgs-2105;
+      registry.nixpkgs2111.flake = nixpkgs-2111;
 
       nixPath = [
         "nixpkgs=${nixpkgsPath}"
+        "nixpkgs2105=${nixpkgs2105Path}"
+        "nixpkgs2111=${nixpkgs2111Path}"
+        "/nix/var/nix/profiles/per-user/root/channels"
       ];
     };
 
     systemd.tmpfiles.rules = [
-      "L+ ${nixpkgsPath} - - - - ${unstable}"
+      "L+ ${nixpkgsPath}     - - - - ${unstable}"
+      "L+ ${nixpkgs2105Path} - - - - ${nixpkgs-2105}"
+      "L+ ${nixpkgs2111Path} - - - - ${nixpkgs-2111}"
     ];
   };
 }
