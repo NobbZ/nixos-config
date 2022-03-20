871b9b2269968d432c81c402c11a508b712951f9
Norbert Melzer <timmelzer@gmail.com>
2022-03-20 10:10:52 +0100

    purge oldstuff folder

--- oldstuff/home/nix/default.nix
+++
@@ -1,8 +0,0 @@
-let
-  sources = import ./sources.nix {};
-in [
-  (_: _: {inherit sources;})
-  (import sources.mozilla-overlay.outPath)
-  (import sources.emacs-overlay.outPath)
-  (import ./myOverlay)
-]
