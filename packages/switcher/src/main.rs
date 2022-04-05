392de8f80aaab60eba97f6dba16c519c751e0b93
Norbert Melzer <timmelzer@gmail.com>
2022-04-05 08:07:13 +0200

    add logging

--- packages/switcher/src/main.rs
+++ packages/switcher/src/main.rs
@@ -1,54 +1,80 @@
 use futures::future;
-use std::{error::Error, fmt::Display, io::Error as IoError, path::Path, process::ExitStatus, str};
+use std::{
+    error::Error,
+    fmt::{Debug, Display},
+    io::Error as IoError,
+    path::Path,
+    process::ExitStatus,
+    str,
+};
 use tokio::{self, process::Command};
+use tracing::field;
+use tracing::{instrument, Level};
+use tracing_futures::Instrument;
+use tracing_subscriber::FmtSubscriber;
 
 const OWNER: &str = "nobbz";
 const REPO: &str = "nixos-config";
 
 const BRANCH: &str = "main";
 
+#[instrument]
 async fn get_command_out(cmd: &mut Command) -> String {
     let out = cmd.output().await.unwrap().stdout;
 
     str::from_utf8(&out).unwrap().trim().to_string()
 }
 
+#[instrument]
 async fn spawn_command(cmd: &mut Command) -> Result<ExitStatus, IoError> {
     cmd.spawn().unwrap().wait().await
 }
 
+#[instrument]
 async fn retrieve_sha<S1, S2, S3>(owner: S1, repo: S2, branch: S3) -> String
 where
-    S1: Display,
-    S2: Display,
-    S3: Display,
+    S1: Display + Debug,
+    S2: Display + Debug,
+    S3: Display + Debug,
 {
     let endpoint = format!("/repos/{}/{}/commits/{}", owner, repo, branch);
 
     get_command_out(Command::new("gh").args(["api", &endpoint, "--jq", ".sha"])).await
 }
 
+#[instrument]
 async fn get_hostname() -> String {
     get_command_out(&mut Command::new("hostname")).await
 }
 
+#[instrument]
 async fn get_username() -> String {
     get_command_out(&mut Command::new("whoami")).await
 }
 
+#[instrument]
 async fn get_tempfldr() -> String {
     get_command_out(&mut Command::new("mktemp").arg("-d")).await
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    let _subscriber = FmtSubscriber::builder().with_max_level(Level::INFO).init();
+
+    tracing::info!("Gathering info");
+
     let sha1_promise = retrieve_sha(OWNER, REPO, BRANCH);
     let host_promise = get_hostname();
     let user_promise = get_username();
     let temp_promise = get_tempfldr();
 
     let (sha1, host, user, temp) =
-        future::join4(sha1_promise, host_promise, user_promise, temp_promise).await;
+        future::join4(sha1_promise, host_promise, user_promise, temp_promise)
+            .instrument(tracing::trace_span!("join4"))
+            .await;
+
+    tracing::info!(%sha1, %host, %user, %temp, "Gathered info");
+    tracing::info!("Building strings");
 
     let flake_url = format!("github:{}/{}?ref={}", OWNER, REPO, sha1);
     let nixos_config = format!("{}#nixos/config/{}", flake_url, host);
@@ -56,6 +82,9 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let home_config = format!("{}#home/config/{}@{}", flake_url, user, host);
     let home_manager = format!("{}#{}@{}", flake_url, user, host);
     let out_link = Path::new(&temp).join("result");
+
+    tracing::info!(%flake_url, %nixos_config, %nixos_rebuild, %home_config, %home_manager, ?out_link, "Built strings");
+    tracing::info!("Starting to build");
 
     spawn_command(Command::new("nix").args([
         "build",
@@ -68,6 +97,9 @@ async fn main() -> Result<(), Box<dyn Error>> {
     ]))
     .await?;
 
+    tracing::info!("Finished building");
+    tracing::info!(%host, "Switching system configuration");
+
     spawn_command(Command::new("sudo").args([
         "nixos-rebuild",
         "switch",
@@ -76,7 +108,13 @@ async fn main() -> Result<(), Box<dyn Error>> {
     ]))
     .await?;
 
+    tracing::info!(%host, "Switched system configuration");
+    tracing::info!(%user, %host, "Switching user configuration");
+
     spawn_command(Command::new("home-manager").args(["switch", "--flake", &home_manager])).await?;
+
+    tracing::info!(%user, %host, "Switched user configuration");
+    tracing::info!(%temp, "Cleaning up");
 
     spawn_command(Command::new("rm").args(["-rfv", &temp])).await?;
 
