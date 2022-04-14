e6a0cc73c7554ecd3c9f3f4d185794f0d037fdce
Norbert Melzer <timmelzer@gmail.com>
2022-04-14 20:00:40 +0200

    version bump

--- packages/switcher/default.nix
+++ packages/switcher/default.nix
@@ -9,18 +9,21 @@
   nixos-rebuild,
   home-manager,
   ncurses6,
+  pkg-config,
+  openssl
 }: let
   runtimeDeps = [gh hostname coreutils nix nixos-rebuild home-manager ncurses6];
 in
   rustPlatform.buildRustPackage {
     pname = "nobbz-switcher";
-    version = "0.1.0";
+    version = "0.2.0";
 
-    nativeBuildInputs = [makeWrapper];
+    nativeBuildInputs = [makeWrapper pkg-config];
+    buildInputs = [openssl];
 
     src = ./.;
 
-    cargoSha256 = "sha256-KROqjfumSwyj1MSeq8fvc3k4twIKor8XSPhg3o5MjS8=";
+    cargoSha256 = "sha256-m8IlajRMidedjm+eyKJdasAOtrgZ6dTtaqYl1nJxLE8=";
 
     postInstall = ''
       wrapProgram $out/bin/switcher \
