c591d2b331f639bb4837d4f57e08431372fc1131
Norbert Melzer <timmelzer@gmail.com>
2022-05-14 14:57:49 +0200

    enceladeus: use most recent unstable

--- nixos/configurations/default.nix
+++ nixos/configurations/default.nix
@@ -1,5 +1,5 @@
 {self, ...} @ inputs: {
   mimas = self.lib.mkSystem "mimas" inputs.unstable; # nixpkgs-2105;
-  enceladeus = self.lib.mkSystem "enceladeus" inputs.unstable;
+  enceladeus = self.lib.mkSystem "enceladeus" inputs.nixpkgs;
   thetys = self.lib.mkSystem "thetys" inputs.unstable;
 }
