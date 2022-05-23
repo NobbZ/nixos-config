20673436bcc013171ac5c856a97d9edf4e310b52
Norbert Melzer <timmelzer@gmail.com>
2022-05-23 07:30:52 +0200

    remove thetys from home manager

--- home/configurations/default.nix
+++ home/configurations/default.nix
@@ -1,5 +1,4 @@
 {self, ...} @ inputs: {
   "nmelzer@mimas" = self.lib.mkHome "nmelzer" "mimas" "x86_64-linux" inputs.unstable;
   "nmelzer@enceladeus" = self.lib.mkHome "nmelzer" "enceladeus" "x86_64-linux" inputs.unstable;
-  "demo@thetys" = self.lib.mkHome "demo" "thetys" "x86_64-linux" inputs.unstable;
 }
