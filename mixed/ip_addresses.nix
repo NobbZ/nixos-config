03910d868ce9ffd19c137c7a483188599897dd22
Norbert Melzer <timmelzer@gmail.com>
2022-05-24 22:45:13 +0200

    enceladeus: fix typo in hostname

--- mixed/ip_addresses.nix
+++ mixed/ip_addresses.nix
@@ -1,7 +1,7 @@
 _: {...}: {
   lib.nobbz = {
-    enceladeaus.v4 = "172.24.199.101";
-    enceladeaus.v6 = "fcc5:ee64:a023:8429:3ae6:0000:0000:0001";
+    enceladeus.v4 = "172.24.199.101";
+    enceladeus.v6 = "fcc5:ee64:a023:8429:3ae6:0000:0000:0001";
 
     rhea.v4 = "72.24.2.1";
     rhea.v6 = "fcc5:ee64:a0b7:9bb6:2ed8:0000:0000:0001";
