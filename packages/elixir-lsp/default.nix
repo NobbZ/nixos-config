b44f42ecf757e438ec1d8baa444eab34acdf8272
Norbert Melzer <timmelzer@gmail.com>
2022-06-16 22:26:05 +0200

    elixir-ls: make use of mkDerivation with function arg

--- packages/elixir-lsp/default.nix
+++ packages/elixir-lsp/default.nix
@@ -13,34 +13,36 @@
 
   source = builtins.fromJSON (builtins.readFile ./source.json);
 in
-  stdenv.mkDerivation rec {
+  stdenv.mkDerivation (self: {
     name = "elixir-ls";
     version = "${source.version}-${erlang.version}-${elixir.version}";
 
-    nativeBuildInputs = [elixir hex git deps cacert];
+    nativeBuildInputs = [elixir hex git cacert];
+    buildInputs = [self.deps];
 
     deps = fetchMixDeps {
-      name = "${name}-${version}";
-      inherit src;
+      name = "${self.name}-${self.version}";
+      inherit (self) src;
       sha256 = "sha256-8MOV2a/C5uO5Q1S97XY8VP0bJI4ByFRIIHNwRtG94cs=";
     };
 
     src = fetchFromGitHub rec {
-      name = "source-${owner}-${repo}-${version}";
+      name = "source-${owner}-${repo}-${self.version}";
       inherit (source) owner repo rev sha256;
     };
 
     dontStrip = true;
 
+    HEX_OFFLINE = "1";
+    HEX_HOME = "/build/${self.src.name}/hex";
+    MIX_ENV = "prod";
+    MIX_HOME = "/build/${self.src.name}";
+    MIX_REBAR3 = "${rebar3}/bin/rebar3";
+    REBAR_GLOBAL_CONFIG_DIR = "/build/${self.src.name}/rebar3";
+    REBAR_CACHE_DIR = "/build/§{self.src.name}/rebar3.cache";
+
     configurePhase = ''
-      export MIX_ENV=prod
-      export HEX_OFFLINE=1
-      export HEX_HOME="$PWD/hex"
-      export MIX_HOME="$PWD"
-      export MIX_REBAR3="${rebar3}/bin/rebar3"
-      export REBAR_GLOBAL_CONFIG_DIR="$PWD/rebar3"
-      export REBAR_CACHE_DIR="$PWD/rebar3.cache"
-      cp --no-preserve=all -R ${deps} deps
+      cp --no-preserve=all -R ${self.deps} deps
       mix deps.compile --no-deps-check
     '';
 
@@ -61,4 +63,4 @@ in
         --replace "ERL_LIBS=\"\$SCRIPTPATH:\$ERL_LIBS\"" \
                   "ERL_LIBS=$out/lib:\$ERL_LIBS"
     '';
-  }
+  })
