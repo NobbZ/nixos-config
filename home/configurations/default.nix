ab1ad101c93c76ddb7bf0572464b75e55a532fc6
Norbert Melzer <nmelzer@Titan.local>
2022-06-22 13:25:00 +0200

    rename the apple VM

--- home/configurations/default.nix
+++ home/configurations/default.nix
@@ -1,5 +1,5 @@
 {self, ...} @ inputs: {
   "nmelzer@mimas" = self.lib.mkHome "nmelzer" "mimas" "x86_64-linux" inputs.unstable;
   "nmelzer@enceladeus" = self.lib.mkHome "nmelzer" "enceladeus" "x86_64-linux" inputs.unstable;
-  "nmelzer@Virtuelle-Maschine-von-Norbert.local" = self.lib.mkHome "nmelzer" "Virtuelle-Maschine-von-Norbert" "aarch64-darwin" inputs.unstable;
+  "nmelzer@Titan.local" = self.lib.mkHome "nmelzer" "titan" "aarch64-darwin" inputs.unstable;
 }
