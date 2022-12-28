03da11e800f38f91a20e317f885babcb21218645
Norbert Melzer <timmelzer@gmail.com>
2022-12-28 10:45:05 +0100

    fix: statix check

--- checks/statix.nix
+++ checks/statix.nix
@@ -5,5 +5,5 @@
 }:
 runCommand "statix-run-${self.rev or "00000000"}" {} ''
   cd ${self}
-  ${statix}/bin/statix check -i packages/nodePackages/node-env.nix | tee $out
+  ${statix}/bin/statix check -i packages/nodePackages/node-env.nix npins/default.nix | tee $out
 ''
