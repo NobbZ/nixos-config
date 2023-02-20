96547775ba039ca06bd04af5448b6dc2f356a187
Norbert Melzer <timmelzer@gmail.com>
2023-02-20 23:53:22 +0100

    chore: nest inputs

--- flake.nix
+++ flake.nix
@@ -1,36 +1,38 @@
 {
-  inputs.nixpkgs-2105.url = "github:nixos/nixpkgs/nixos-21.05";
-  inputs.nixpkgs-2111.url = "github:nixos/nixpkgs/nixos-21.11";
-  inputs.nixpkgs-2205.url = "github:nixos/nixpkgs/nixos-22.05";
-  inputs.nixpkgs-2211.url = "github:nixos/nixpkgs/nixos-22.11";
-  inputs.unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-  inputs.master.url = "github:nixos/nixpkgs/master";
-  inputs.nixpkgs-insync.url = "github:nixos/nixpkgs/bd751508cf67db3b13b03e25eb937854fc92ee30";
+  inputs = {
+    nixpkgs-2105.url = "github:nixos/nixpkgs/nixos-21.05";
+    nixpkgs-2111.url = "github:nixos/nixpkgs/nixos-21.11";
+    nixpkgs-2205.url = "github:nixos/nixpkgs/nixos-22.05";
+    nixpkgs-2211.url = "github:nixos/nixpkgs/nixos-22.11";
+    unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    master.url = "github:nixos/nixpkgs/master";
+    nixpkgs-insync.url = "github:nixos/nixpkgs/bd751508cf67db3b13b03e25eb937854fc92ee30";
 
-  inputs.parts.url = "github:hercules-ci/flake-parts";
+    parts.url = "github:hercules-ci/flake-parts";
 
-  inputs.programsdb.url = "github:wamserma/flake-programs-sqlite";
-  inputs.programsdb.inputs.nixpkgs.follows = "unstable";
+    programsdb.url = "github:wamserma/flake-programs-sqlite";
+    programsdb.inputs.nixpkgs.follows = "unstable";
 
-  # The following is required to make flake-parts work.
-  inputs.nixpkgs.follows = "unstable";
+    # The following is required to make flake-parts work.
+    nixpkgs.follows = "unstable";
 
-  inputs.nix.url = "github:nixos/nix?ref=pull/7856/head";
+    nix.url = "github:nixos/nix?ref=pull/7856/head";
 
-  inputs.nil.url = "github:oxalica/nil";
+    nil.url = "github:oxalica/nil";
 
-  inputs.home-manager.url = "github:nix-community/home-manager";
-  inputs.home-manager.inputs.nixpkgs.follows = "unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "unstable";
 
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+    flake-utils.url = "github:numtide/flake-utils";
 
-  inputs.emacs.url = "github:nix-community/emacs-overlay";
-  inputs.emacs.inputs.nixpkgs.follows = "master";
+    emacs.url = "github:nix-community/emacs-overlay";
+    emacs.inputs.nixpkgs.follows = "master";
 
-  inputs.nixos-vscode-server.url = "github:msteen/nixos-vscode-server";
+    nixos-vscode-server.url = "github:msteen/nixos-vscode-server";
 
-  inputs.statix.url = "github:nerdypepper/statix";
-  inputs.alejandra.url = "github:kamadorueda/alejandra/3.0.0";
+    statix.url = "github:nerdypepper/statix";
+    alejandra.url = "github:kamadorueda/alejandra/3.0.0";
+  };
 
   outputs = {
     self,
