bc35107e2fc7b7dbe7fe8490b61291cde238dd51
Norbert Melzer <timmelzer@gmail.com>
2023-03-15 07:42:45 +0100

    refactor: move p10k into own module

--- home/modules/profiles/base/default.nix
+++ home/modules/profiles/base/default.nix
@@ -72,6 +72,7 @@ in {
       home-manager.enable = true;
       htop.enable = true;
       jq.enable = true;
+      p10k.enable = true;
 
       ssh = {
         enable = true;
@@ -192,16 +193,6 @@ in {
             name = "nix-zsh-complete.zsh";
             src = zsh-complete;
             file = "_nix";
-          }
-          {
-            name = "powerlevel10k";
-            src = pkgs.zsh-powerlevel10k;
-            file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-          }
-          {
-            name = "powerlevel10k-config";
-            src = ./p10k-config;
-            file = "p10k.zsh";
           }
           {
             name = "zsh-syntax-highlighting";
