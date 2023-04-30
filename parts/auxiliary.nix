ed66f38b021a622d613a252408f2a75a59e11991
Norbert Melzer <timmelzer@gmail.com>
2023-04-30 11:36:47 +0200

    chore: add an 'app' that rotates keys

--- parts/auxiliary.nix
+++ parts/auxiliary.nix
@@ -9,6 +9,23 @@
   }: {
     formatter = self.packages.${system}.alejandra;
 
+    apps.rotate.program = let
+      sopsrotate = pkgs.writeShellScript "sops-rotate" ''
+        file=$1
+
+        printf "Rotating %s...\n" "''${file}"
+        ${pkgs.sops}/bin/sops -r -i "''${file}"
+      '';
+      rotate = pkgs.writeShellScript "rotate" ''
+        ${pkgs.git}/bin/git -c rotate-$(${pkgs.coreutils}/bin/date -Idate) >/dev/null || true
+
+        ${pkgs.findutils}/bin/find secrets \( -name '*.yaml' -o -name '*.yml' \) -a -type f -exec ${sopsrotate} '{}' \;
+
+        ${pkgs.git}/bin/git add secrets
+        ${pkgs.git}/bin/git commit -m "chore: rotate secrets $(${pkgs.coreutils}/bin/date -Idate)"
+      '';
+    in "${rotate}";
+
     devShells.default = pkgs.mkShell {
       packages = builtins.attrValues {
         inherit (self'.packages) nil alejandra;
