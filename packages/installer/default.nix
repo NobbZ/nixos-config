35d9294bb5a08fc4ce4fc7b3e996dae6ef728013
Norbert Melzer <timmelzer@gmail.com>
2023-08-06 10:40:25 +0200

    feature: use awesome for installer

--- packages/installer/default.nix
+++ packages/installer/default.nix
@@ -8,16 +8,26 @@ nixosSystem {
   modules = [
     ({modulesPath, ...}: {
       imports = [
-        "${modulesPath}/installer/cd-dvd/installation-cd-graphical-plasma5.nix"
+        "${modulesPath}/installer/cd-dvd/installation-cd-graphical-base.nix"
       ];
+
+      isoImage = {
+        edition = "nobbz";
+        squashfsCompression = "zstd -Xcompression-level 10";
+      };
+
+      services.xserver = {
+        windowManager.awesome.enable = true;
+        displayManager.sddm.enable = true;
+        displayManager.autoLogin.enable = true;
+        displayManager.autoLogin.user = "nixos";
+      };
 
       services.lvm = {
         boot.thin.enable = true;
         boot.vdo.enable = true;
         dmeventd.enable = true;
       };
-
-      isoImage.squashfsCompression = "zstd -Xcompression-level 10";
     })
   ];
 }
