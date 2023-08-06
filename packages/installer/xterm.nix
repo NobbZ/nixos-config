16b4f8fb66a4fdfcffb629efebe16db6e67bc7fe
Norbert Melzer <timmelzer@gmail.com>
2023-08-06 15:01:20 +0200

    refactor: use user-tempfile rule

--- packages/installer/xterm.nix
+++ packages/installer/xterm.nix
@@ -1,15 +1,14 @@
 {pkgs, ...}: let
   inherit (pkgs) writeText;
 in {
-  systemd.tmpfiles.rules = let
+  systemd.user.tmpfiles.rules = let
     xresources = writeText "xresources" ''
       XTerm.vt100.foreground: rgb:d3/d3/d3
       XTerm.vt100.background: rgb:00/00/00
       XTerm.vt100.faceName: FreeMono
       XTerm.vt100.faceSize: 12
     '';
-    home = "/home/nixos";
   in [
-    "L+ ${home}/.Xresources - - - - ${xresources}"
+    "L+ %h/.Xresources - - - - ${xresources}"
   ];
 }
