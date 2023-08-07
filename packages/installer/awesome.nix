380533764f2d3537c152e086569cfe2bc5591536
Norbert Melzer <timmelzer@gmail.com>
2023-08-07 08:23:56 +0200

    feature: awesome configuration added

--- packages/installer/awesome.nix
+++ packages/installer/awesome.nix
@@ -1,18 +1,35 @@
 {
   pkgs,
   npins,
+  wrapper-manager,
   ...
 }: let
-  awesome = pkgs.awesome.overrideAttrs (oa: {
-    version = npins.awesome.revision;
-    src = npins.awesome;
+  rc_lua = pkgs.runCommandNoCC "awesomerc.lua" {} ''
+    substitute ${./awesomerc.lua} $out \
+      --subst-var-by FILE_PATH_WALLPAPER ${./nix-glow-black.png}
+  '';
+  awesome = wrapper-manager.lib.build {
+    inherit pkgs;
 
-    patches = [];
+    modules = [
+      {
+        wrappers.awesome = {
+          basePackage = pkgs.awesome.overrideAttrs (oa: {
+            version = npins.awesome.revision;
+            src = npins.awesome;
 
-    postPatch = ''
-      patchShebangs tests/examples/_postprocess.lua
-    '';
-  });
+            patches = [];
+
+            postPatch = ''
+              patchShebangs tests/examples/_postprocess.lua
+            '';
+          });
+
+          flags = ["--config" "${rc_lua}"];
+        };
+      }
+    ];
+  };
 in {
   _file = ./awesome.nix;
 
