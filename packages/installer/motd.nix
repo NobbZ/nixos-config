026e8381692b16b7896557aa5826d9f38db60c55
Norbert Melzer <timmelzer@gmail.com>
2023-08-24 10:13:25 +0200

    chore: provide actual MOTD

--- packages/installer/motd.nix
+++ packages/installer/motd.nix
@@ -1,19 +1,40 @@
 {
   pkgs,
-  config,
   lib,
   ...
 }: {
   _file = ./motd.nix;
 
-  users.motd = "This is a test message.";
+  environment.etc."gui-motd".text = ''
+    Welcome to NobbZ' <b>inofficial</b> NixOS live disk!
+
+    The tooling, configuration and drivers on this disk are mostly tailored to
+    my own needs and might not work well for you.
+
+    Currently you are looking at an <tt>awesome wm</tt> session, the modkey is
+    <tt>Mod4</tt> (the "windows" key).
+
+    <s>Most important tooling is available through the "awesome" menu, which is</s>
+    <s>located in the upper left corner of the screen.</s>
+
+    If you want to use the terminal, you can either use the menu, or press
+    <tt>Mod4+Enter</tt>.
+
+    <s>Rofi is available as well, you can open it by pressing <tt>Mod4+d</tt>.</s>
+
+    A more complete list of keybindings can be found in the awesome help screen
+    by pressing <tt>Mod4+s</tt>.
+  '';
 
   systemd.user.services.motd = {
     description = "Prints a test message";
     wantedBy = ["graphical-session.target"];
+    script = ''
+      motd=$(cat /etc/gui-motd)
+      ${lib.getExe pkgs.zenity} --info --text="$motd" --no-wrap --title="Short usage notes"
+    '';
     serviceConfig = {
       Type = "simple";
-      ExecStart = "${lib.getExe pkgs.zenity} --info --text='${config.users.motd}'";
       Restart = "on-failure";
     };
   };
