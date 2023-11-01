e6d0628cf36a5f0efe501fbeb13731a6a94fef4e
Norbert Melzer <timmelzer@gmail.com>
2023-11-01 10:33:10 +0100

    feature(janus): some more user setup

--- home/configurations/nmelzer_at_janus.nix
+++ home/configurations/nmelzer_at_janus.nix
@@ -1,1 +1,20 @@
-_: _: {}
+_: {pkgs, ...}: {
+  _file = ./nmelzer_at_mimas.nix;
+
+  nixpkgs.allowedUnfree = ["google-chrome" "vscode" "discord" "obsidian"];
+
+  activeProfiles = ["browsing" "development"];
+
+  dconf.enable = true;
+
+  home.packages = builtins.attrValues {
+    inherit (pkgs) keepassxc nix-output-monitor discord obsidian vscode;
+  };
+
+  xsession.windowManager.awesome.autostart = [
+    "${pkgs.blueman}/bin/blueman-applet"
+    "${pkgs.networkmanagerapplet}/bin/nm-applet"
+  ];
+
+  services.playerctld.enable = true;
+}
