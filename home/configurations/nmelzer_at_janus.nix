0ee107b7d11f07e0efd12d3b0e79f7ad40509c0d
Norbert Melzer <timmelzer@gmail.com>
2023-11-01 14:13:29 +0100

    feature(janus): set up git\* secrets

--- home/configurations/nmelzer_at_janus.nix
+++ home/configurations/nmelzer_at_janus.nix
@@ -10,6 +10,20 @@ in {
   sops.age.sshKeyPaths = ["${sshConfigPath}/id_ed25519"];
   sops.defaultSopsFile = "${self}/secrets/janus/nmelzer/default.yaml";
 
+  sops.secrets."github" = {
+    path = "${sshConfigPath}/github";
+    mode = "0400";
+    sopsFile = "${self}/secrets/users/nmelzer/github";
+    format = "binary";
+  };
+
+  sops.secrets."gitlab" = {
+    path = "${sshConfigPath}/gitlab";
+    mode = "0400";
+    sopsFile = "${self}/secrets/users/nmelzer/gitlab";
+    format = "binary";
+  };
+
   dconf.enable = true;
 
   home.packages = builtins.attrValues {
