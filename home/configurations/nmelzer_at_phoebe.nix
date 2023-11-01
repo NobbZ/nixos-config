f526d92a0ef60477c707966c4acc93f69f71b17e
Norbert Melzer <timmelzer@gmail.com>
2023-11-01 15:50:08 +0100

    feature(phoebe): initial HM (broken secrets)

---
+++ home/configurations/nmelzer_at_phoebe.nix
@@ -0,0 +1,36 @@
+{self, ...}: {config, pkgs, lib, ...}: let
+  sshConfigPath = "${config.home.homeDirectory}/.ssh";
+in {
+  _file = ./nmelzer_at_phoebe.nix;
+
+  nixpkgs.allowedUnfree = ["google-chrome" "vscode" "discord" "obsidian" "slack"];
+
+  activeProfiles = ["browsing" "development"];
+
+  sops.age.sshKeyPaths = ["${sshConfigPath}/id_ed25519"];
+  sops.defaultSopsFile = "${self}/secrets/phoebe/nmelzer/default.yaml";
+
+  sops.secrets."github" = {
+    path = "${sshConfigPath}/github";
+    mode = "0400";
+    sopsFile = "${self}/secrets/users/nmelzer/github";
+    format = "binary";
+  };
+
+  sops.secrets."gitlab" = {
+    path = "${sshConfigPath}/gitlab";
+    mode = "0400";
+    sopsFile = "${self}/secrets/users/nmelzer/gitlab";
+    format = "binary";
+  };
+
+  dconf.enable = true;
+
+  home.packages = builtins.attrValues {
+    inherit (pkgs) keepassxc nix-output-monitor discord obsidian vscode slack;
+  };
+
+  xsession.windowManager.awesome.enable = lib.mkForce false;
+
+  services.playerctld.enable = true;
+}
