f65260bea1dab7e0331f44d384b03c2f9bb5ddf5
Norbert Melzer <timmelzer@gmail.com>
2023-11-13 07:09:01 +0100

    fix: rotate was missing non-YAML files

--- parts/auxiliary.nix
+++ parts/auxiliary.nix
@@ -19,9 +19,9 @@
         ${pkgs.sops}/bin/sops -r -i "''${file}"
       '';
       rotate = pkgs.writeShellScript "rotate" ''
-        ${pkgs.git}/bin/git -c rotate-$(${pkgs.coreutils}/bin/date -Idate) >/dev/null || true
+        ${pkgs.git}/bin/git switch -c rotate-$(${pkgs.coreutils}/bin/date -Idate) >/dev/null || true
 
-        ${pkgs.findutils}/bin/find secrets \( -name '*.yaml' -o -name '*.yml' \) -a -type f -exec ${sopsrotate} '{}' \;
+        ${pkgs.findutils}/bin/find secrets -a -type f -exec ${sopsrotate} '{}' \;
 
         ${pkgs.git}/bin/git add secrets
         ${pkgs.git}/bin/git commit -m "chore: rotate secrets $(${pkgs.coreutils}/bin/date -Idate)"
