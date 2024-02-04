db69eb72ab93903bdfd69652d91f064a2cf26555
Norbert Melzer <timmelzer@gmail.com>
2024-02-04 10:55:09 +0100

    kernel version explained in enceladeus

--- nixos/configurations/enceladeus.nix
+++ nixos/configurations/enceladeus.nix
@@ -27,7 +27,7 @@ _: {
   ];
 
   services.lvm.boot.thin.enable = true;
-  boot.kernelPackages = pkgs.linuxPackages_6_6; # TODO: Figure why I have this and nothing newer?
+  boot.kernelPackages = pkgs.linuxPackages_6_6; # WiFi kernel module doesn't build on newer kernels
   services.lvm.dmeventd.enable = true;
   boot.blacklistedKernelModules = ["rtl8xxxu"];
   boot.extraModulePackages = with config.boot.kernelPackages; [
