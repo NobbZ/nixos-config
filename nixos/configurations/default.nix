b3fc068998083c4ef418252c1605aa0d3cb2c968
Norbert Melzer <timmelzer@gmail.com>
2024-03-02 13:24:31 +0100

    remove remainders of janus

--- nixos/configurations/default.nix
+++ nixos/configurations/default.nix
@@ -4,5 +4,4 @@ _: {
   nobbz.nixosConfigurations.mimas.system = "x86_64-linux";
   nobbz.nixosConfigurations.enceladeus.system = "x86_64-linux";
   nobbz.nixosConfigurations.hyperion.system = "aarch64-linux";
-  nobbz.nixosConfigurations.janus.system = "x86_64-linux";
 }
