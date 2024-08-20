47f018ad2a75260dccffdf1cf0537a4670e0f19f
Norbert Melzer <timmelzer@gmail.com>
2024-08-20 21:04:33 +0200

    add pre-rust update input

--- flake.nix
+++ flake.nix
@@ -30,6 +30,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     nixpkgs-insync-v3.url = "github:nixos/nixpkgs?ref=32fdc268e921994e3f38088486ddfe765d11df93";
+    nixpkgs-pre-rust.url = "github:nixos/nixpkgs?ref=57d0d4a8f302";
 
     nvim.url = "github:nobbz/nobbz-vim";
     nvim.inputs.parts.follows = "parts";
