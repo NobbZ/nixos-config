d4b4145dd5a8c003dcea64eb95436ee59abde460
Norbert Melzer <timmelzer@gmail.com>
2024-08-20 21:04:59 +0200

    use zerotier from before the rust update

--- nixos/modules/zerotier.nix
+++ nixos/modules/zerotier.nix
@@ -1,8 +1,19 @@
-_: {
+{nixpkgs-pre-rust, ...}: {
+  config,
+  pkgs,
+  ...
+}: {
   _file = ./zerotier.nix;
 
   services.zerotierone.enable = true;
   services.zerotierone.joinNetworks = ["8286ac0e4768c8ae"];
 
   services.zerotierone.localConf = {};
+
+  services.zerotierone.package =
+    (import nixpkgs-pre-rust {
+      inherit (config.nixpkgs) config;
+      inherit (pkgs) system;
+    })
+    .zerotierone;
 }
