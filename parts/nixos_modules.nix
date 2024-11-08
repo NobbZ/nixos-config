640e324f18488cb18967490c01e5835a28831659
Norbert Melzer <timmelzer@gmail.com>
2024-11-08 07:52:44 +0100

    extract submodule into binding

--- parts/nixos_modules.nix
+++ parts/nixos_modules.nix
@@ -19,43 +19,45 @@
     // lib.optionalAttrs (builtins.any (p: p module) [isPath isString]) {_file = module;};
 
   modules = builtins.mapAttrs (_: config: config.wrappedModule) cfg;
+
+  submodule = lib.types.submodule ({
+    name,
+    config,
+    ...
+  }: {
+    options = {
+      name = lib.mkOption {
+        type = lib.types.str;
+        default = name;
+      };
+
+      module = lib.mkOption {
+        type = lib.types.oneOf [
+          lib.types.str
+          lib.types.path
+          # TODO: add sets and functions
+        ];
+      };
+
+      extraArgs = lib.mkOption {
+        type = lib.types.nullOr (lib.types.attrsOf lib.types.unspecified);
+        default = inputs;
+      };
+
+      wrappedModule = lib.mkOption {
+        type = lib.types.unspecified;
+        readOnly = true;
+      };
+    };
+
+    config = {
+      wrappedModule = callModule config.module config.extraArgs;
+    };
+  });
 in {
   options = {
     nobbz.nixosModules = lib.mkOption {
-      type = lib.types.attrsOf (lib.types.submodule ({
-        name,
-        config,
-        ...
-      }: {
-        options = {
-          name = lib.mkOption {
-            type = lib.types.str;
-            default = name;
-          };
-
-          module = lib.mkOption {
-            type = lib.types.oneOf [
-              lib.types.str
-              lib.types.path
-              # TODO: add sets and functions
-            ];
-          };
-
-          extraArgs = lib.mkOption {
-            type = lib.types.nullOr (lib.types.attrsOf lib.types.unspecified);
-            default = inputs;
-          };
-
-          wrappedModule = lib.mkOption {
-            type = lib.types.unspecified;
-            readOnly = true;
-          };
-        };
-
-        config = {
-          wrappedModule = callModule config.module config.extraArgs;
-        };
-      }));
+      type = lib.types.attrsOf submodule;
     };
   };
 
