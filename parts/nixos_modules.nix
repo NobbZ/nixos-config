4c1bc513f6b0c7e9f29066f4ae8b7b055e28c463
Norbert Melzer <timmelzer@gmail.com>
2024-11-08 08:03:10 +0100

    Extract helpers into own file

--- parts/nixos_modules.nix
+++ parts/nixos_modules.nix
@@ -7,53 +7,9 @@
 }: let
   cfg = config.nobbz.nixosModules;
 
-  inherit (builtins) isString isPath;
-
-  callModule = module: args: let
-    moduleToImport =
-      if args == null
-      then module
-      else import module args;
-  in
-    {imports = [moduleToImport];}
-    // lib.optionalAttrs (builtins.any (p: p module) [isPath isString]) {_file = module;};
+  inherit (import ./module_helpers.nix lib inputs) submodule;
 
   modules = builtins.mapAttrs (_: config: config.wrappedModule) cfg;
-
-  submodule = lib.types.submodule ({
-    name,
-    config,
-    ...
-  }: {
-    options = {
-      name = lib.mkOption {
-        type = lib.types.str;
-        default = name;
-      };
-
-      module = lib.mkOption {
-        type = lib.types.oneOf [
-          lib.types.str
-          lib.types.path
-          # TODO: add sets and functions
-        ];
-      };
-
-      extraArgs = lib.mkOption {
-        type = lib.types.nullOr (lib.types.attrsOf lib.types.unspecified);
-        default = inputs;
-      };
-
-      wrappedModule = lib.mkOption {
-        type = lib.types.unspecified;
-        readOnly = true;
-      };
-    };
-
-    config = {
-      wrappedModule = callModule config.module config.extraArgs;
-    };
-  });
 in {
   options = {
     nobbz.nixosModules = lib.mkOption {
