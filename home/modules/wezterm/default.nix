eda4d0f89e0da841584a7c6b0d17e2c8ca1211d2
Norbert Melzer <timmelzer@gmail.com>
2025-03-01 19:28:03 +0100

    do wezterm via HM

---
+++ home/modules/wezterm/default.nix
@@ -0,0 +1,45 @@
+_: {
+  config,
+  pkgs,
+  ...
+}: {
+  home.packages = [pkgs.wezterm];
+
+  xdg.configFile."wezterm/wezterm.lua".text =
+    # lua
+    ''
+      -- Pull in the wezterm API
+      local wezterm = require 'wezterm'
+
+      -- This table will hold the configuration.
+      local config = {}
+
+      -- In newer versions of wezterm, use the config_builder which will
+      -- help provide clearer error messages
+      if wezterm.config_builder then
+        config = wezterm.config_builder()
+      end
+
+      -- This is where you actually apply your config choices
+
+      -- For example, changing the color scheme:
+      config.color_scheme = "Catppuccin Mocha"
+
+      -- disable ligatures
+      config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
+
+      -- set the font
+      config.font_dirs = { '/home/nmelzer/.nix-profile/share/fonts/otf' }
+      config.font_size = 11.0 * 1.25
+      config.font = wezterm.font("Departure Mono")
+
+      -- setting up keybindings
+      config.keys = {
+        -- The default is `C-Z` (so also pressing SHIFT), I prefer to not have SHIFT pressed
+        { key = 'z', mods = 'CTRL', action = wezterm.action.TogglePaneZoomState, },
+      }
+
+      -- and finally, return the configuration to wezterm
+      return config
+    '';
+}
