a4081a3fb277d92f542a5681eb2a45c05f4935ec
Norbert Melzer <timmelzer@gmail.com>
2026-01-02 00:58:03 +0100

    refactor expression

--- packages/advcp/default.nix
+++ packages/advcp/default.nix
@@ -4,23 +4,23 @@
   sources,
   upstream ? "coreutils",
 }:
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (self: {
   name = "advcp";
   version = "9.5";
   strictDeps = true;
 
   src = fetchurl {
-    name = "source-${name}-${version}.tar.xz";
-    url = "ftp://ftp.gnu.org/gnu/${upstream}/${upstream}-${version}.tar.xz";
+    name = "source-${self.name}-${self.version}.tar.xz";
+    url = "ftp://ftp.gnu.org/gnu/${upstream}/${upstream}-${self.version}.tar.xz";
     hash = "sha256-zTKO3qyS9qZl3p8yPJO3Eq8YWLwuDYjz9xAEaUcKG4o=";
   };
 
   patches = [
-    "${sources.advcpmv}/advcpmv-0.9-${version}.patch"
+    "${sources.advcpmv}/advcpmv-0.9-${self.version}.patch"
   ];
 
   installPhase = ''
     install -D src/cp $out/bin/advcp
     install -D src/mv $out/bin/advmv
   '';
-}
+})
