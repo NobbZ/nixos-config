6044ff5ebb75556f74b9f776eecac3177ba5ff96
Norbert Melzer <timmelzer@gmail.com>
2026-01-02 22:24:32 +0100

    mimas: initial searxng setup

---
+++ nixos/configurations/mimas/searx.nix
@@ -0,0 +1,32 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  sops.secrets.searx = {};
+
+  services.searx = {
+    enable = true;
+
+    package = pkgs.searxng;
+
+    domain = "search.mimas.internal.nobbz.dev";
+
+    environmentFile = config.sops.secrets.searx.path;
+  };
+
+  services.traefik.dynamicConfigOptions.http = {
+    routers.searx = {
+      entryPoints = ["http" "https"];
+      rule = "Host(`${config.services.searx.domain}`)";
+      service = "searx";
+      tls.domains = [{main = "*.mimas.internal.nobbz.dev";}];
+      tls.certResolver = "mimasWildcard";
+    };
+
+    services.searx.loadBalancer = {
+      passHostHeader = true;
+      servers = [{url = "http://localhost:8888";}];
+    };
+  };
+}
