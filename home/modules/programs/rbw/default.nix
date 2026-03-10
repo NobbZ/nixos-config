002428dd2087cb2eefa5de4d5d072018ffa57094
Norbert Melzer <timmelzer@gmail.com>
2026-03-10 23:02:20 +0100

    Fix rbw config

--- home/modules/programs/rbw/default.nix
+++ home/modules/programs/rbw/default.nix
@@ -5,14 +5,15 @@ _: {
   ...
 }: let
   cfg = config.programs.rbw;
+  base_url = "https://passwords.mimas.internal.nobbz.dev";
 in {
   config = lib.mkIf cfg.enable {
     programs.rbw.settings = {
       pinentry = pkgs.pinentry-curses;
 
       email = "timmelzer@gmail.com";
-      base_url = "https://passwords.mimas.internal.nobbz.dev";
-      ui_url = cfg.settings.base_url;
+      base_url = base_url;
+      ui_url = base_url;
     };
   };
 }
